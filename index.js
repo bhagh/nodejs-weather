900476d5256a3d11664f304cbe5eb10b57062b87
bhagh <haghb1@gmail.com>
2019-10-04 23:57:12 -0400

    Install yargs

--- index.js
+++ index.js
@@ -2,16 +2,22 @@ require('dotenv').config()
 
 
 const request = require('request');
-request(url, function (err, response, body) {
-  if(err){
-    console.log('error:', error);
-  } else {
-    console.log('body:', body);
-  }
-});
+const argv = require('yargs').argv;
+
 
 let apiKey = process.env.apiKey;
-let city = 'raleigh';
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+let city = argv.c || 'raleigh';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
 
-console.log(apiKey)
+request(url, function (err, response, body) {
+    if(err){
+      console.log('error:', error);
+    } else {
+      let weather = JSON.parse(body)
+      let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+      console.log(message);
+    }
+  });
+
+
+
